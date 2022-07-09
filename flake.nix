6c3007aa651c6058549ceea838461d0cb0304f81
bs2k <bs2k@bs2k.me>
2022-07-10 01:14:55 +0900

    flake

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "Like /dev/urandom, but objectively better";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "nixpkgs/nixos-21.05";
+
+  outputs = { self, nixpkgs, kernel, kmod }:
+    let
+
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+
+    in
+
+    {
+
+      # A Nixpkgs overlay.
+      overlay = final: prev: {
+
+        uwurandom = with final; stdenv.mkDerivation rec {
+          name = "uwurandom-${version}-${kernel.version}";
+
+          src = ./.;
+
+          sourceRoot = "source/";
+          hardeningDisable = [ "pic" "format" ];
+          nativeBuildInputs = kernel.moduleBuildDependencies;
+
+          makeFlags = [
+            "KERNELRELEASE=${kernel.modDirVersion}"
+            "KERNEL_DIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+            "INSTALL_MOD_PATH=$(out)"
+          ];
+        };
+
+      };
+
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        {
+          inherit (nixpkgsFor.${system}) uwurandom;
+        });
+
+      # The default package for 'nix build'. This makes sense if the
+      # flake provides only one package or there is a clear "main"
+      # package.
+      defaultPackage = forAllSystems (system: self.packages.${system}.uwurandom);
+
+    };
+}
