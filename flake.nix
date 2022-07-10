f432317289813a3156bba0eae0ef0ce41363e9dc
bs2k <bs2k@bs2k.me>
2022-07-10 11:39:19 +0900

    dsadsfjfds

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Like /dev/urandom, but objectively better";
 
   # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "nixpkgs/nixos-21.05";
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.11";
 
   outputs = { self, nixpkgs }:
     let
@@ -19,9 +19,42 @@
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+
     in
 
     {
+      overlay = final: prev: {
+
+        uwurandom = kernel: with final; stdenv.mkDerivation rec {
+          name = "uwurandom-${version}-${kernel.version}";
+
+          src = ./.;
+
+          sourceRoot = "source/";
+          hardeningDisable = [ "pic" "format" ];
+          nativeBuildInputs = kernel.moduleBuildDependencies;
+
+          makeFlags = [
+            "KERNELRELEASE=${kernel.modDirVersion}"
+            "KERNEL_DIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+            "INSTALL_MOD_PATH=$(out)"
+          ];
+        };
+
+      };
+
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        {
+          inherit (nixpkgsFor.${system}) uwurandom;
+        });
+
+      # The default package for 'nix build'. This makes sense if the
+      # flake provides only one package or there is a clear "main"
+      # package.
+      defaultPackage = forAllSystems (system: self.packages.${system}.uwurandom);
 
       nixosModules.uwurandom =
         { pkgs, config, ... }:
@@ -29,31 +62,9 @@
           kernel = config.boot.kernelPackages.kernel;
         in
         {
-          nixpkgs.overlays = [
-            (
-              final: prev: {
+          nixpkgs.overlays = [ self.overlay ];
 
-                uwurandom = with final; stdenv.mkDerivation rec {
-                  name = "uwurandom-${version}-${kernel.version}";
-
-                  src = ./.;
-
-                  sourceRoot = "source/";
-                  hardeningDisable = [ "pic" "format" ];
-                  nativeBuildInputs = kernel.moduleBuildDependencies;
-
-                  makeFlags = [
-                    "KERNELRELEASE=${kernel.modDirVersion}"
-                    "KERNEL_DIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
-                    "INSTALL_MOD_PATH=$(out)"
-                  ];
-                };
-
-              }
-            )
-          ];
-
-          boot.extraModulePackages = [ pkgs.uwurandom ];
+          boot.extraModulePackages = [ pkgs.uwurandom { kernel = config.boot.kernelPackages.kernel } ];
         };
 
     };
